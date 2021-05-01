b3240580184486dcca41d3b7dab4f208598fbb4b
Mark Loveland <mdl2665@login3.stampede2.tacc.utexas.edu>
2021-04-30 21:18:24 -0500

    added cmake module

---
+++ build/CMakeFiles/atmesh.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/atmesh.dir/src/atmesh.F90.o"
+  "CMakeFiles/atmesh.dir/src/atmesh_mod.F90.o"
+  "libatmesh.a"
+  "libatmesh.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang Fortran)
+  include(CMakeFiles/atmesh.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/atmesh.dir/cmake_clean_Fortran.cmake
@@ -0,0 +1,10 @@
+# Remove fortran modules provided by this target.
+FILE(REMOVE
+  "atmesh.mod"
+  "ATMESH.mod"
+  "CMakeFiles/atmesh.dir/atmesh.mod.stamp"
+
+  "atmesh_mod.mod"
+  "ATMESH_MOD.mod"
+  "CMakeFiles/atmesh.dir/atmesh_mod.mod.stamp"
+  )
